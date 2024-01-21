9d44e0abf21dc132dbd38c9a29949ad5ff6c48de
0xRomRom <roman0811@live.nl>
2024-01-22 00:36:10 +0100

    Dropdown configured

--- ledsgoneonform/src/components/navoverlay/NavOverlay.jsx
+++ ledsgoneonform/src/components/navoverlay/NavOverlay.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 const NavOverlay = () => {
   const [hoveredState, setHoveredState] = useState(null);
+  const [showDroplist, setShowDroplist] = useState(false);
 
   return (
     <div className={stl.navOverlay}>
@@ -29,6 +30,7 @@ const NavOverlay = () => {
               Home
             </a>
           </li>
+
           <li
             className={stl.navLink}
             onMouseOver={() => setHoveredState("Tekst Samenstellen")}
@@ -39,24 +41,121 @@ const NavOverlay = () => {
               className={`${stl.anchor} ${
                 hoveredState === "Tekst Samenstellen" ? stl.active : ""
               }`}
-              href="https://ledsgoneon.nl/"
+              href="https://ledsgoneon.nl/tekst-samenstellen/"
               rel="noreferrer"
-              onClick={() => (window.location.href = "https://ledsgoneon.nl")}
+              onClick={() =>
+                (window.location.href =
+                  "https://ledsgoneon.nl/tekst-samenstellen/")
+              }
             >
-              Home
+              Tekst Samenstellen
             </a>
           </li>
-          <li>
-            <a className={stl.navLink}></a>
+
+          <li
+            className={stl.navLink}
+            onMouseOver={() => setHoveredState("Logo Samenstellen")}
+            onMouseLeave={() => setHoveredState(null)}
+          >
+            <FaArrowRight className={stl.arrowright} />
+            <a
+              className={`${stl.anchor} ${
+                hoveredState === "Logo Samenstellen" ? stl.active : ""
+              }`}
+              href="https://ledsgoneon.nl/logo-samenstellen/"
+              rel="noreferrer"
+              onClick={() =>
+                (window.location.href =
+                  "https://ledsgoneon.nl/logo-samenstellen/")
+              }
+            >
+              Logo Samenstellen
+            </a>
           </li>
-          <li>
-            <a className={stl.navLink}></a>
+
+          <li
+            className={stl.navLink}
+            onMouseOver={() => setHoveredState("Projecten")}
+            onMouseLeave={() => setHoveredState(null)}
+          >
+            <FaArrowRight className={stl.arrowright} />
+            <a
+              className={`${stl.anchor} ${
+                hoveredState === "Projecten" ? stl.active : ""
+              }`}
+              href="https://ledsgoneon.nl/projecten/"
+              rel="noreferrer"
+              onClick={() =>
+                (window.location.href = "https://ledsgoneon.nl/projecten/")
+              }
+            >
+              Projecten
+            </a>
           </li>
-          <li>
-            <a className={stl.navLink}></a>
+
+          <li
+            className={stl.navLink}
+            onMouseOver={() => setHoveredState("Inspiratie")}
+            onMouseLeave={() => setHoveredState(null)}
+          >
+            <FaArrowRight className={stl.arrowright} />
+            <a
+              className={`${stl.anchor} ${
+                hoveredState === "Inspiratie" ? stl.active : ""
+              }`}
+              href="https://ledsgoneon.nl/inspiratie/"
+              rel="noreferrer"
+              onClick={() =>
+                (window.location.href = "https://ledsgoneon.nl/inspiratie/")
+              }
+            >
+              Inspiratie
+            </a>
           </li>
-          <li>
-            <a className={stl.navLink}></a>
+
+          <li
+            className={`${stl.navLink} ${stl.overons} ${
+              showDroplist ? stl.rotate : ""
+            }`}
+            onMouseOver={() => setHoveredState("Over ons")}
+            onMouseLeave={() => setHoveredState(null)}
+          >
+            <FaArrowRight className={stl.arrowright} />
+            <a
+              className={`${stl.anchor} ${
+                hoveredState === "Over ons" ? stl.active : ""
+              }`}
+              onClick={() => setShowDroplist(!showDroplist)}
+            >
+              Over ons
+              <div
+                className={`${stl.droplist} ${
+                  showDroplist ? stl.showDrop : ""
+                }`}
+              >
+                <a
+                  className={stl.innerDropItem}
+                  href="https://ledsgoneon.nl/over-ons/"
+                  rel="noreferrer"
+                  onClick={() =>
+                    (window.location.href = "https://ledsgoneon.nl/over-ons/")
+                  }
+                >
+                  Over ons
+                </a>
+                <a
+                  className={stl.innerDropItem}
+                  href="https://ledsgoneon.nl/productieproces/"
+                  rel="noreferrer"
+                  onClick={() =>
+                    (window.location.href =
+                      "https://ledsgoneon.nl/productieproces/")
+                  }
+                >
+                  Productieproces
+                </a>
+              </div>
+            </a>
           </li>
         </ul>
       </div>
